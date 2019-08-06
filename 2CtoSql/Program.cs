b4d5fc4881cb3f83280db20d4e91149e0a8767bf
max-student@outlook.com <max-student@outlook.com>
2019-08-05 22:15:36 -0400

    added orders

--- 2CtoSql/Program.cs
+++ 2CtoSql/Program.cs
@@ -6,23 +6,43 @@ using System.Data.SqlClient;
 namespace _2CtoSql {
     class Program {
         static void Main(string[] args) {
+
             var sql = "SELECT * from Customers where State = 'OH';";
             var customers = SelectCustomer(sql);
             foreach (var customer in customers) {
                 Console.WriteLine(customer.Name);
             }
+
             sql = "SELECT * from Orders;";
             var orders = SelectOrder(sql);
-            foreach (var order in orders) {
-                Console.WriteLine(order.Id);
+            foreach (var Orders in orders) {
+                Console.WriteLine(orders);
+            }
+
+            var cust = GetCustomerById("2001");
+            if (cust == null) {
+                Console.WriteLine("Customer not found");
+            }
+            else {
+                Console.WriteLine(cust.Name);
+            }
+
+            var ord = GetOrderById("2001");
+            if (ord == null) {
+                Console.WriteLine("Order not found");
+            }
+            else {
+                Console.WriteLine(ord);
             }
         }
+
         static List<Order> SelectOrder(string sql) {
             var connStr = @"server=localhost\sqlexpress;database=CustomerOrderDb;trusted_connection=true;";
             var connection = new SqlConnection(connStr);
             connection.Open();
             if (connection.State != System.Data.ConnectionState.Open) {
-                throw new Exception("Connection did not open!"); }
+                throw new Exception("Connection did not open!");
+            }
             var orderList = new List<Order>();
             var cmd = new SqlCommand(sql, connection);
             var reader = cmd.ExecuteReader();
@@ -53,7 +73,8 @@ namespace _2CtoSql {
             var connection = new SqlConnection(connStr);
             connection.Open();
             if (connection.State != System.Data.ConnectionState.Open) {
-                throw new Exception("Connection did not open!"); }
+                throw new Exception("Connection did not open!");
+            }
             var customerList = new List<Customer>();
             var cmd = new SqlCommand(sql, connection);
             var reader = cmd.ExecuteReader();
@@ -74,7 +95,69 @@ namespace _2CtoSql {
             connection.Close();
             return customerList;
         }
-      }
+        static Customer GetCustomerById(string pid) {
+            var connStr = @"server=localhost\sqlexpress;database=CustomerOrderDb;trusted_connection=true;";
+            var connection = new SqlConnection(connStr);
+            connection.Open();
+            if (connection.State != System.Data.ConnectionState.Open) {
+                throw new Exception("Connection did not open!");
+            }
+            var sql = "SELECT * From Customers where Id = @myid;";
+            var cmd = new SqlCommand(sql, connection);
+            var customerId = new SqlParameter("@myid", pid);
+            cmd.Parameters.Add(customerId);
+            var reader = cmd.ExecuteReader();
+            Customer cust = null;
+            if (reader.Read()) {
+                var id = (int)reader["Id"];
+                var name = reader["Name"].ToString();
+                var city = reader["City"].ToString();
+                var state = reader["State"].ToString();
+                var active = (bool)reader["Active"];
+                var code = reader.IsDBNull(reader.GetOrdinal("Code"))
+                       ? null
+                       : reader["Code"].ToString();
+                cust = new Customer(id, name, city, state, active, code);
+            }
+            reader.Close();
+            connection.Close();
+            return cust;
+        }
+        static Order GetOrderById(string pid) {
+            var connStr = @"server=localhost\sqlexpress;database=CustomerOrderDb;trusted_connection=true;";
+            var connection = new SqlConnection(connStr);
+            connection.Open();
+            if (connection.State != System.Data.ConnectionState.Open) {
+                throw new Exception("Connection did not open!");
+            }
+            var sql = "SELECT * From Orders where Id = @myid;";
+            var cmd = new SqlCommand(sql, connection);
+            var orderId = new SqlParameter("@myid", pid);
+            cmd.Parameters.Add(orderId);
+            var reader = cmd.ExecuteReader();
+            Order orders = null;
+            if (reader.Read()) {
+                var id = (int)reader["Id"];
+                var date = (DateTime)reader["Date"];
+                var note = reader.IsDBNull(reader.GetOrdinal("Note"))
+                       ? null
+                       : reader["Note"].ToString();
+                int customerid;
+                if (reader.IsDBNull(reader.GetOrdinal("CustomerId"))) {
+                    customerid = 0;
+                }
+                else {
+                    customerid = (int)reader["CustomerId"];
+                }
+                orders = new Order(id, date, note, customerid);
+
+
+            }
+            reader.Close();
+            connection.Close();
+            return orders;
+        }
     }
+}
 
 
